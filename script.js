3c56c5ee6f53d387275903e356da5d44fa1732b9
Nikodem Zacharewicz <64557018+HiToMiZu@users.noreply.github.com>
2025-08-05 21:32:21 +0200

    Refactor project listing to use radio buttons for repository selection in Projects.html and script.js

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
-fetch('https://api.github.com/users/HiToMiZu/repos')
-.then(res =>  res.json())
-.then(data => {
-    data.forEach(repo => {
-        console.log(repo)
-        const list = document.querySelector('ul')
-        const nameofrepo = document.createElement('li');
-        nameofrepo.innerText = repo.name;
-        list.append(nameofrepo);
+class Repo {
+    constructor(site) {
+        this.site = site;
+    }
+}
 
+const repoInstances = {};
+
+fetch('https://api.github.com/users/HiToMiZu/repos')
+.then(res => res.json())
+.then(data => {
+    const form = document.getElementById('repoForm');
+
+    data.forEach(repoData => {
+        const name = repoData.name;
+
+        // Create and store Repo instance
+        repoInstances[name] = new Repo(repoData.html_url);
+
+        // Create radio input
+        const label = document.createElement('label');
+        const radio = document.createElement('input');
+        radio.type = 'radio';
+        radio.name = 'repo';
+        radio.value = name;
+        label.innerText = name;
+
+        // ✅ Dynamic change listener
+        radio.addEventListener('change', () => {
+            const instance = repoInstances[name];
+            console.log(`You selected: ${name}`);
+            console.log(`Repo URL: ${instance.site}`);
+        });
+
+        label.appendChild(radio);
+        form.appendChild(label);
     });
-})
+});
